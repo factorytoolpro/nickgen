e4dddf37aa31f56426472d18ec2023bcf2a662d8
FactoryToolPro <Factorytoolpro@gmail.com>
2026-04-27 20:21:54 +0100

    clarity1

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import Script from "next/Script";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -44,12 +44,14 @@ export default function RootLayout({ children }) {
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
     <body className="min-h-full flex flex-col">
        <Script id="microsoft-clarity" strategy="afterInteractive">
+    {`
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "wi7kkq9vgi");
-</Script>
+    `}
+    </Script>
         {children}
         <Analytics />
       </body>
