c78d91bbcecc30e965438a3dbe091f7a8a979ede
FactoryToolPro <Factorytoolpro@gmail.com>
2026-04-27 20:00:25 +0100

    clarity

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
-import script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
