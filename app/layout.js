25f40740652742b252feebcbd1916866e68c8d33
FactoryToolPro <Factorytoolpro@gmail.com>
2026-04-27 12:19:09 +0100

    clarity

--- app/layout.js
+++ app/layout.js
@@ -42,14 +42,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <script type="text/javascript">
+    <body className="min-h-full flex flex-col">
+       <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "wi7kkq9vgi");
-</script>
-      <body className="min-h-full flex flex-col">
+</script> 
         {children}
         <Analytics />
       </body>
