07eea4c3f2faf862732852d8d555dfb0c0029f61
FactoryToolPro <Factorytoolpro@gmail.com>
2026-04-27 13:51:37 +0100

    clarity

--- app/layout.js
+++ app/layout.js
@@ -43,13 +43,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
     <body className="min-h-full flex flex-col">
-       <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "wi7kkq9vgi");
-</script> 
+       <Script id="clarity" strategy="afterInteractive">
+{`
+(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "wi7kkq9vgi");
+`}
+</Script>
         {children}
         <Analytics />
       </body>
